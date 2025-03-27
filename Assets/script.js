e9db590c79061a89dc5662ba85e378ac72a95502
Blauwe-Zebra <100291174+Blauwe-Zebra@users.noreply.github.com>
2025-03-27 19:07:11 +0100

    begin script

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,14 @@
-document.getElementById("Number").value = "€0 ";
+let NumberValue = document.getElementById("Number");
+NumberValue.value = "€0 ";
+
+let CurrentInput = null;
+let CurrentOperation = "";
+let PreviousInput = null;
+
+function NumberPress(Number) {
+  if (PreviousInput == null) {
+    CurrentInput = Number;
+  } else {
+    PreviousInput = Number;
+  }
+}
